a7dd2b260f9456f27033ffb31190255b9d49e5e2
Marcelo Alves <marcelo.miguel.as@gmail.com>
2024-01-07 16:04:52 -0300

    test

--- fotos/models.py
+++ fotos/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 import os
 
 class Foto(models.Model):
-    foto = models.ImageField(upload_to='./lembrancas/fotos/static/images/')
+    foto = models.ImageField(upload_to='./fotos/static/images/')
     titulo = models.CharField(max_length=100)
     texto = models.CharField(max_length=1000)
     data = timezone.now()
