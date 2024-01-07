c43c9e92e1d0dc3621ecc94b0559d4a9117a17de
Marcelo Alves <marcelo.miguel.as@gmail.com>
2024-01-07 18:06:15 -0300

    static correct

--- lembrancas/settings.py
+++ lembrancas/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
+    # 'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,7 +132,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = 'staticfiles'
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 MEDIA_URL = ''
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
 # Default primary key field type
